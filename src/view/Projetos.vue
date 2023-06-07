6a8425ecbd755518976858e35445b6f64eb32937
Jones Lima <jonesrasta@gmail.com>
2023-06-07 11:33:58 -0300

    criando lista de projetos

--- src/view/Projetos.vue
+++ src/view/Projetos.vue
@@ -23,10 +23,9 @@
         </tr>
       </thead>
       <tbody>
-        
-            <tr v-for= "projeto in projetos" :key="projeto.id">
-            <td>{{ projeto.id }}</td>
-            <td>{{ projeto.nome }}</td>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <td>{{ projeto.id }}</td>
+          <td>{{ projeto.nome }}</td>
         </tr>
       </tbody>
     </table>
@@ -34,27 +33,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import IProjeto from "../interfaces/IProjeto";
+import { computed, defineComponent } from "vue";
+import { useStore } from "@/store";
+
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Projetos",
   data() {
     return {
       nomeDoProjeto: "",
-      projetos: [] as IProjeto[],
     };
   },
   methods: {
     salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: new Date().toISOString(),
-      };
-
-      this.projetos.push(projeto);
+      this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
       this.nomeDoProjeto = "";
     },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+      projetos: computed(() => store.state.projetos),
+    };
   },
 });
 </script>

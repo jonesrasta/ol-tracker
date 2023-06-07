57b18966715dcbbe4c53c433bdd4252c8bfab505
Jones Lima <jonesrasta@gmail.com>
2023-06-07 10:19:05 -0300

    adicionando store

--- src/components/FormularioCont.vue
+++ src/components/FormularioCont.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
-        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao" />
+      <div
+        class="column is-5"
+        role="form"
+        aria-label="Formulario para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="column">
         <TemporizadorCont @aoTemporizadorFinalizado="finalizarTarefa" />
@@ -12,37 +35,46 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import TemporizadorCont from './TemporizadorCont.vue'
+import { computed, defineComponent } from "vue";
+import TemporizadorCont from "./TemporizadorCont.vue";
+import { useStore } from "vuex";
+
+import { key } from "@/store";
 
 export default defineComponent({
   name: "FormularioCont",
-  emits: ['aoSalvarListaTarefas'],
+  emits: ["aoSalvarListaTarefas"],
   components: {
-    TemporizadorCont
+    TemporizadorCont,
   },
   data() {
     return {
-      descricao: ''
-    }
+      descricao: "",
+      idProjeto: "",
+    };
   },
   methods: {
     finalizarTarefa(tempoDecorrido: number): void {
-      this.$emit('aoSalvarListaTarefas', {
+      this.$emit("aoSalvarListaTarefas", {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
-
-      })
-      this.descricao = ''
-    }
-  }
-
+        descricao: this.descricao,
+        projeto: this.projetos.find((proj) => proj.id == this.idProjeto),
+      });
+      this.descricao = "";
+    },
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
 });
 </script>
 
 <style>
-  .formulario {
-    color: var(--texto-primario);
-    background-color: var(--bg-primario);
-  }
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
 </style>
